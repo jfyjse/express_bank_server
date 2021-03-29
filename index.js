892c5304ddcd549a525740d3e01d3b69f94f5aab
jfyjse <joffy.jose.jfy@gmail.com>
2021-03-29 17:18:55 +0530

    login, withdraw without login npermitt

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const {json} = require('express');
 const express = require('express');
 const app = express();
-
+const session = require('express-session')
 const dataser= require('./service/data.service')
 
 app.use(express.json());
-
+app.use(session(
+{
+    secret:'randomsecurestring',
+    resave:false,
+    saveUninitialized:false
+})
+)
 app.get('/',(req,res)=>{
     res.status(222).send("get methd updated")
 })
@@ -13,15 +19,40 @@ app.get('/',(req,res)=>{
 app.post('/register',(req,res)=>
 {
     console.log(req.body);
-    const result= dataser.register(req.body.accno,req.body.password)
-    console.log(res.send(result.message));
+    const result= dataser.register(req.body.accno,req.body.pwd)
+    // console.log(res.send(result.message));
+    res.status(result.statusCode)
+    console.log(res.json(result));
     
 })
 app.post('/login',(req,res)=>
 {
     console.log(req.body);
-    const result= dataser.login(req.body.accno,req.body.pwd)
-    console.log(res.send(result.message));
+    console.log(req.session.currentUser);
+    const result= dataser.login(req,req.body.accno,req.body.pwd)
+    // console.log(res.send(result.message));
+    res.status(result.statusCode)
+    console.log(res.json(result));
+    
+})
+
+app.post('/deposit',(req,res)=>
+{
+    console.log(req.body);
+    const result= dataser.deposit(req,req.body.accno,req.body.pwd,req.body.amt)
+    // console.log(res.send(result.message));
+    res.status(result.statusCode)
+    console.log(res.json(result));
+    
+})
+
+app.post('/withdraw',(req,res)=>
+{
+    console.log(req.body);
+    const result= dataser.withdraw(req.body.accno,req.body.pwd,req.body.amt)
+    // console.log(res.send(result.message));
+    res.status(result.statusCode)
+    console.log(res.json(result));
     
 })
 
--- service/data.service.js
+++ service/data.service.js
@@ -1,92 +1,199 @@
-let accountDetails= {
-    1000: { accno: 1000, name: "kanappi", balance: 6000, password: "user1" },
-    1001: { accno: 1001, name: "dybala", balance: 9000, password: "user2" },
-    1002: { accno: 1002, name: "kuttappan", balance: 7000, password: "user3" },
-    1003: { accno: 1003, name: "kuttappi", balance: 8000, password: "user4" },
-    1004: { accno: 1004, name: "jinjin", balance: 61000, password: "user5" },
-  }
+let accountDetails = {
+  1000: { accno: 1000, name: "kanappi", balance: 6000, password: "user1" },
+  1001: { accno: 1001, name: "dybala", balance: 9000, password: "user2" },
+  1002: { accno: 1002, name: "kuttappan", balance: 7000, password: "user3" },
+  1003: { accno: 1003, name: "kuttappi", balance: 8000, password: "user4" },
+  1004: { accno: 1004, name: "jinjin", balance: 61000, password: "user5" },
+}
 
 let currentUser;
 
-const  register = (accno, password) => {
+const register = (accno, password) => {
 
-    console.log("register called");
-    
-    if (accno in accountDetails) {
+  console.log("register called");
+
+  if (accno in accountDetails) {
     //   console.log("user exist log in");
     //   alert("please login")
-     
-      return{
-          status:false,
-          message:"user exist log in"
-      } 
 
-    }
-
-    accountDetails[accno] = {
-      accno,
-      name: "new user",
-      balance: 0,
-      password
-    }
-
-    
-    // alert("reg success")
-    // console.log(this.accountDetails);
-    // this.saveDetails()
-    
     return {
-        status:true,
-        message:"registered suuc"
+      status: false,
+      statusCode: 445,
+      message: "user exist log in"
     }
 
   }
 
-  const login= (accno,pwd)=>{
+  accountDetails[accno] = {
+    accno,
+    name: "new useer",
+    balance: 0,
+    password
+  }
 
-    var data = accountDetails;
 
-    if (accno in data) {
-      var psw1 = data[accno].password;
-      if (pwd == psw1) {
-        
-        // console.log("login success");
-        var userl= data[accno].name;
-        currentUser=userl;
-        // currentUser=userl;
+  // alert("reg success")
+  // console.log(this.accountDetails);
+  // this.saveDetails()
+
+  return {
+    status: true,
+    statusCode: 200,
+    message: "registered suuc"
+  }
+
+}
+
+const login = (req, accno, pwd) => {
+
+  var data = accountDetails;
+
+  if (accno in data) {
+    var psw1 = data[accno].password;
+    if (pwd == psw1) {
+
+      // console.log("login success");
+      // var userl = data[accno].name;
+      req.session.currentUser = data[accno].name;
+      // currentUser=userl;
+      return {
+        status: true,
+        statusCode: 200,
+        message: "login suuc"
+      }
+
+
+    }
+    else {
+      // console.log("inncorrect pswd");
+      // return false;
+      return {
+        status: false,
+        statusCode: 445,
+        message: "pwd error"
+      }
+
+
+    }
+  }
+  else {
+    //   console.log("no user account");
+    //   return false;
+    return {
+      status: false,
+      statusCode: 445,
+      message: "no acc"
+    }
+
+
+  }
+}
+
+
+const deposit = (req, accno, pwd, amt) => {
+  if (!req.session.currentUser) {
+    return {
+      status: false,
+      statusCode: 441,
+      message: "pls login"
+    }
+
+  }
+  var ammt = parseInt(amt)
+  var data = accountDetails;
+
+  if (accno in data) {
+    var psw1 = data[accno].password;
+    if (pwd == psw1) {
+
+      console.log("login success");
+      data[accno].balance += ammt;
+
+
+      // alert("acc credited wit :"+amt+" new bal= " + data[accno].balance) 
+      return {
+        status: true,
+        statusCode: 200,
+        message: "credited " + amt + " bal " + data[accno].balance
+      }
+
+
+    }
+    else {
+      // console.log("inncorrect pswd");
+      return {
+        status: false,
+        statusCode: 445,
+        message: "pwd err"
+      }
+
+
+    }
+  }
+  else {
+    // console.log("no user account");
+    return {
+      status: false,
+      statusCode: 445,
+      message: "pwd err"
+    }
+
+
+  }
+
+}
+
+const withdraw = (accno, pwd, amt) => {
+  var ammt = parseInt(amt)
+  var data = accountDetails;
+
+  if (accno in data) {
+    var psw1 = data[accno].password;
+    if (pwd == psw1) {
+
+      // console.log("login success");
+      if ((data[accno].balance) < ammt) {
         return {
-            status:true,
-            message:"login suuc"
+          status: false,
+          statusCode: 445,
+          message: "no money"
         }
-    
-
       }
       else {
-        // console.log("inncorrect pswd");
-        // return false;
-        return{
-            status:false,
-            message:"pwd error"
-        } 
-  
-
+        data[accno].balance -= ammt;
+        // alert("acc debited wit :"+amt+" new bal= " + data[accno].balance);
+        return {
+          status: true,
+          statusCode: 200,
+          message: "debited " + amt + " bal " + data[accno].balance
+        }
       }
     }
     else {
-    //   console.log("no user account");
-    //   return false;
-    return{
-        status:false,
-        message:"no acc"
-    } 
+      // console.log("inncorrect pswd");
+      return {
+        status: false,
+        statusCode: 445,
+        message: "pwd err"
+      }
 
 
     }
   }
+  else {
+    // console.log("no user account");
+    return {
+      status: false,
+      statusCode: 445,
+      message: "no accc"
+    }
 
 
-
-  module.exports=
-  {
-      register, login
   }
+}
+
+
+module.exports =
+{
+  register, login, deposit, withdraw
+}
